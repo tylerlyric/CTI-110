a6a4084f5ea6de39641ecd96b577fb36283c04fb
tylerlyric <tylerl9505@student.faytechcc.edu>
2025-06-15 20:35:55 -0400

    Delete P1LAB1_TylerLyric.py

--- P1LAB1_TylerLyric.py
+++
@@ -1,8 +0,0 @@
-# Lyric Tyler
-# June 11th, 2025
-# P1Lab1
-# Create a program that allows the user to enter their first and last name
-
-first_name = input("Enter your first name: ")
-last_name = input("Enter your last name: ")
-print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
